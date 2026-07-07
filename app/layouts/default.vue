e3d1726d2d7d9565fbb771bee0caa3ff422fa2ce
Massimiliano Mauro <info@pianeta.studio>
2026-07-07 13:25:38 +0200

    feat(segnale): aggiunge widget segnalazioni al sito
    Crea GitHub Issue su PianetaDev/barbara-costantini per bug/richieste
    segnalate dal team.

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,3 +1,15 @@
+<script setup lang="ts">
+// Widget segnalazioni "Segnale" → crea GitHub Issue su PianetaDev/barbara-costantini
+useHead({
+  script: [{
+    src: 'https://segnale-phi.vercel.app/widget.js',
+    defer: true,
+    'data-project': 'barbara-costantini',
+    'data-source': 'public',
+  }],
+})
+</script>
+
 <template>
   <div class="min-h-screen flex flex-col bg-bc-canvas">
     <BcNav />
